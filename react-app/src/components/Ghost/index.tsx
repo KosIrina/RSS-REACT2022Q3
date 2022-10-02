bc6b8d7e2c2af5ccf09bd3be944f3c3ffa0c6331
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-03 00:50:33 +0300

    feat: add ghost component for 404 page and styles to it

---
+++ react-app/src/components/Ghost/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Ghost.css';
+
+const Ghost = () => {
+  return (
+    <>
+      <div className="ghost-legs">
+        <div className="one" />
+        <div className="two" />
+        <div className="three" />
+        <div className="four" />
+      </div>
+      <div className="ghost">
+        <div className="face">
+          <div className="eye-left" />
+          <div className="eye-right" />
+          <div className="mouth" />
+        </div>
+      </div>
+      <div className="ghost-shadow" />
+    </>
+  );
+};
+
+export default Ghost;
