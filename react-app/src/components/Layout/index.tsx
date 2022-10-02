103f2137e9ed856f82c22066cf97e7fc7cd4a352
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-03 00:34:54 +0300

    feat: add layout component and styles to it

---
+++ react-app/src/components/Layout/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+import './Layout.css';
+
+const Layout = () => {
+  return (
+    <>
+      <header className="header container">
+        <h1 className="header__logo">Rick and Morty Characters</h1>
+        <nav className="header__navigation navigation">
+          <NavLink className="navigation__link" to="/" end>
+            Main
+          </NavLink>
+          <NavLink className="navigation__link" to="/about">
+            About Us
+          </NavLink>
+        </nav>
+      </header>
+
+      <main className="main container">
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default Layout;
