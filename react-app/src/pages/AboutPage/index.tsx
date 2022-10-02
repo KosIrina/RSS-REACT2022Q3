0eda7b47e17b79683bd9fb1a8b0a3da6110abfaf
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-03 00:38:09 +0300

    feat: add about page and styles to it

---
+++ react-app/src/pages/AboutPage/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './AboutPage.css';
+
+const AboutPage = () => {
+  return (
+    <>
+      <h2 className="about-page__greeting">Welcome to Rick and Morty Characters!</h2>
+      <p className="about-page__app-description">
+        Here you can find information about characters from this universe.
+        <br />
+        Enjoy!
+      </p>
+      <p className="about-page__app-info">
+        Frontend Developer -{' '}
+        <a className="about-page__gihtub-link" href="https://github.com/KosIrina">
+          Irina Kosyanchuk
+        </a>
+        <br />© 2022
+      </p>
+    </>
+  );
+};
+
+export default AboutPage;
