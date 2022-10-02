6fc9ed294cb4ec141f8bf9436721c242cd82513d
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-03 00:54:17 +0300

    feat: add router to app

--- react-app/src/components/App/index.tsx
+++ react-app/src/components/App/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Layout from '../Layout';
+import MainPage from '../../pages/MainPage';
+import AboutPage from '../../pages/AboutPage';
+import PageNotFound from '../../pages/PageNotFound';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="404" element={<PageNotFound />} />
+          <Route path="*" element={<Navigate to="404" />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
