3dfdc2f232791b9c028e10d1b0499dc521bd43df
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-03 11:09:13 +0300

    feat: add card item component and styles to it

---
+++ react-app/src/components/CardItem/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { CardItemProps } from 'types';
+import './CardItem.css';
+
+const CardItem = (props: CardItemProps): JSX.Element => {
+  return (
+    <li className="main-page__card-item character-card">
+      <img className="character-card__image" src={props.character.image} alt="Character image" />
+      <p className="character-card__name">
+        <i>
+          <b>Name: </b>
+        </i>
+        {props.character.name}
+      </p>
+      <p className="character-card__status">
+        <i>
+          <b>Status: </b>
+        </i>
+        {props.character.status}
+      </p>
+      <p className="character-card__species">
+        <i>
+          <b>Species: </b>
+        </i>
+        {props.character.species}
+      </p>
+      <p className="character-card__gender">
+        <i>
+          <b>Gender: </b>
+        </i>
+        {props.character.gender}
+      </p>
+      <p className="character-card__episodes">
+        <i>
+          <b>Amount of episodes: </b>
+        </i>
+        {props.character.episode.length}
+      </p>
+    </li>
+  );
+};
+
+export default CardItem;
