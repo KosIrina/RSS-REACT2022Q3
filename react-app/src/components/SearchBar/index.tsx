1be6c6f91116db2bb85a0b43ff77a85d364daeea
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-03 11:07:33 +0300

    feat: add search bar, styles to it, work with local storage

---
+++ react-app/src/components/SearchBar/index.tsx
@@ -0,0 +1,39 @@
+import React, { ChangeEvent } from 'react';
+import './SearchBar.css';
+import { SearchBarProps, SearchBarState } from '../../types';
+
+class SearchBar extends React.Component<SearchBarProps, SearchBarState> {
+  constructor(props: SearchBarProps) {
+    super(props);
+    this.state = {
+      inputValue: localStorage.getItem('searchValue') || '',
+    };
+  }
+
+  componentWillUnmount(): void {
+    localStorage.setItem('searchValue', this.state.inputValue);
+  }
+
+  onSearchChange(event: ChangeEvent<HTMLInputElement>): void {
+    this.setState({ inputValue: event.target.value });
+  }
+
+  render() {
+    return (
+      <>
+        <form className="main-page__search-form">
+          <input
+            type="search"
+            className="main-page__search-input"
+            placeholder="Search character.."
+            autoComplete="off"
+            value={this.state.inputValue}
+            onChange={(event: ChangeEvent<HTMLInputElement>): void => this.onSearchChange(event)}
+          />
+        </form>
+      </>
+    );
+  }
+}
+
+export default SearchBar;
