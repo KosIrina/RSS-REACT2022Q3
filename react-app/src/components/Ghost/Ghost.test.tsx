71434c3923736e26f7fa0f416820776af0cbbbc5
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-04 01:47:59 +0300

    feat: add tests for ghost component

---
+++ react-app/src/components/Ghost/Ghost.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Ghost from '.';
+import { Numbers } from '../../constants';
+
+describe('Ghost', (): void => {
+  it('Should render ghost legs', (): void => {
+    render(<Ghost />);
+    expect(screen.getByTestId('ghost-legs')).toBeInTheDocument();
+  });
+
+  it('Should contain the appropriate amount of legs', (): void => {
+    render(<Ghost />);
+    expect(screen.getAllByTestId('ghost-leg').length).toBe(Numbers.Four);
+  });
+
+  it('Should render ghost body', (): void => {
+    render(<Ghost />);
+    expect(screen.getByTestId('ghost-body')).toBeInTheDocument();
+  });
+
+  it('Should render ghost shadow', (): void => {
+    render(<Ghost />);
+    expect(screen.getByTestId('ghost-shadow')).toBeInTheDocument();
+  });
+
+  it('Should render both ghost eyes', (): void => {
+    render(<Ghost />);
+    expect(
+      screen.getByTestId('ghost-left-eye') && screen.getByTestId('ghost-right-eye')
+    ).toBeInTheDocument();
+  });
+});
--- react-app/src/components/Ghost/index.tsx
+++ react-app/src/components/Ghost/index.tsx
@@ -4,20 +4,20 @@ import './Ghost.css';
 const Ghost = (): JSX.Element => {
   return (
     <>
-      <div className="ghost-legs">
-        <div className="one" />
-        <div className="two" />
-        <div className="three" />
-        <div className="four" />
+      <div className="ghost-legs" data-testid="ghost-legs">
+        <div className="one" data-testid="ghost-leg" />
+        <div className="two" data-testid="ghost-leg" />
+        <div className="three" data-testid="ghost-leg" />
+        <div className="four" data-testid="ghost-leg" />
       </div>
-      <div className="ghost">
+      <div className="ghost" data-testid="ghost-body">
         <div className="face">
-          <div className="eye-left" />
-          <div className="eye-right" />
+          <div className="eye-left" data-testid="ghost-left-eye" />
+          <div className="eye-right" data-testid="ghost-right-eye" />
           <div className="mouth" />
         </div>
       </div>
-      <div className="ghost-shadow" />
+      <div className="ghost-shadow" data-testid="ghost-shadow" />
     </>
   );
 };
