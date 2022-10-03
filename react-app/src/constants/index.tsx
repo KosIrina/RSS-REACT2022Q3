ffc5a5f8744aa1de88e5693e792a685218560f29
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-04 01:48:48 +0300

    feat: add some constants

---
+++ react-app/src/constants/index.tsx
@@ -0,0 +1,15 @@
+export const EMPTY_STRING = '';
+
+export const LOCAL_STORAGE_KEYS = {
+  searchValue: 'searchValue',
+};
+
+export const ENTER_KEY_CODES = {
+  enter: 'Enter',
+  enterNumpad: 'NumpadEnter',
+};
+
+export enum Numbers {
+  Zero = 0,
+  Four = 4,
+}
