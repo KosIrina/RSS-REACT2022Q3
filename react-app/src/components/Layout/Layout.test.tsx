cd5b3176c66b043db463d15e9ef2678f820842dc
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-04 01:49:17 +0300

    feat: add tests for layout

---
+++ react-app/src/components/Layout/Layout.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '.';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('Layout', (): void => {
+  it('Should render logo', (): void => {
+    render(<App />, { wrapper: BrowserRouter });
+    expect(screen.getByText(/rick and morty characters/i)).toBeInTheDocument();
+  });
+
+  it('Should render navigation', (): void => {
+    render(<App />, { wrapper: BrowserRouter });
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+  });
+
+  it('Should render main page link', (): void => {
+    render(<App />, { wrapper: BrowserRouter });
+    expect(screen.getByText(/main/i)).toBeInTheDocument();
+  });
+
+  it('Should render about page link', (): void => {
+    render(<App />, { wrapper: BrowserRouter });
+    expect(screen.getByText(/about us/i)).toBeInTheDocument();
+  });
+});
