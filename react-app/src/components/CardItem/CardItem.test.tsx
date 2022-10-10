117d17543dd7966ec4c5b36ae671a72975525665
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-11 02:50:07 +0300

    feat: add new tests for card and card list

--- react-app/src/components/CardItem/CardItem.test.tsx
+++ react-app/src/components/CardItem/CardItem.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import CharactersInfo from '../../data/universe-characters.json';
-import { Numbers } from '../../types';
+import CustomCharactersInfo from '../../data/custom-characters.json';
+import { CustomData, Numbers } from '../../types';
 import CardItem from '.';
 
 describe('CardItem', (): void => {
@@ -38,5 +39,52 @@ describe('CardItem', (): void => {
   it('Should render amount of episodes', (): void => {
     render(<CardItem character={CharactersInfo[Numbers.Zero]} />);
     expect(screen.getByText(/amount of episodes:/i)).toBeInTheDocument();
+  });
+
+  it('Should not render birthdate', (): void => {
+    render(<CardItem character={CharactersInfo[Numbers.Zero]} />);
+    expect(screen.queryByText(/birthday:/i)).toBeNull();
+  });
+});
+
+describe('Custom CardItem', (): void => {
+  it('Should render card', (): void => {
+    render(<CardItem character={(CustomCharactersInfo as CustomData)[Numbers.Zero]} />);
+    expect(screen.getByTestId('main-page-character-card')).toBeInTheDocument();
+  });
+
+  it('Should render image', (): void => {
+    render(<CardItem character={(CustomCharactersInfo as CustomData)[Numbers.Zero]} />);
+    expect(screen.getByAltText(/character image/i)).toBeInTheDocument();
+  });
+
+  it('Should render name', (): void => {
+    render(<CardItem character={(CustomCharactersInfo as CustomData)[Numbers.Zero]} />);
+    expect(screen.getByText(/name:/i)).toBeInTheDocument();
+  });
+
+  it('Should render status', (): void => {
+    render(<CardItem character={(CustomCharactersInfo as CustomData)[Numbers.Zero]} />);
+    expect(screen.getByText(/status:/i)).toBeInTheDocument();
+  });
+
+  it('Should render species', (): void => {
+    render(<CardItem character={(CustomCharactersInfo as CustomData)[Numbers.Zero]} />);
+    expect(screen.getByText(/species:/i)).toBeInTheDocument();
+  });
+
+  it('Should render gender', (): void => {
+    render(<CardItem character={(CustomCharactersInfo as CustomData)[Numbers.Zero]} />);
+    expect(screen.getByText(/gender:/i)).toBeInTheDocument();
+  });
+
+  it('Should render birthday', (): void => {
+    render(<CardItem character={(CustomCharactersInfo as CustomData)[Numbers.Zero]} />);
+    expect(screen.getByText(/birthday:/i)).toBeInTheDocument();
+  });
+
+  it('Should not render amount of episodes', (): void => {
+    render(<CardItem character={(CustomCharactersInfo as CustomData)[Numbers.Zero]} />);
+    expect(screen.queryByText(/amount of episodes:/i)).toBeNull();
   });
 });
--- react-app/src/components/CardList/CardList.test.tsx
+++ react-app/src/components/CardList/CardList.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import CardList from '.';
 import CharactersInfo from '../../data/universe-characters.json';
+import CustomCharactersInfo from '../../data/custom-characters.json';
+import { CustomData } from '../../types';
 
 describe('CardList', (): void => {
   it('Should render cards list', (): void => {
@@ -12,5 +14,19 @@ describe('CardList', (): void => {
   it('Should contain the appropriate amount of cards', (): void => {
     render(<CardList characters={CharactersInfo} />);
     expect(screen.getAllByTestId('main-page-character-card').length).toEqual(CharactersInfo.length);
+  });
+});
+
+describe('Custom CardList', (): void => {
+  it('Should render cards list', (): void => {
+    render(<CardList characters={CustomCharactersInfo as CustomData} />);
+    expect(screen.getByTestId('main-characters-list')).toBeInTheDocument();
+  });
+
+  it('Should contain the appropriate amount of cards', (): void => {
+    render(<CardList characters={CustomCharactersInfo as CustomData} />);
+    expect(screen.getAllByTestId('main-page-character-card').length).toEqual(
+      CustomCharactersInfo.length
+    );
   });
 });
