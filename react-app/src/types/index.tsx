f65db991bcf0100fa244239411483dc26299145e
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-10 20:27:13 +0300

    feat: add types necessary for form page

--- react-app/src/types/index.tsx
+++ react-app/src/types/index.tsx
@@ -1,3 +1,5 @@
+import React, { ChangeEvent } from 'react';
+
 export type SearchBarProps = Record<string, never>;
 
 export interface ISearchBarState {
@@ -25,14 +27,14 @@ export interface IDataElement {
   created: string;
 }
 
-export type IData = IDataElement[];
+export type Data = IDataElement[];
 
 export interface ICardItemProps {
-  character: IDataElement;
+  character: IDataElement | ICustomDataElement;
 }
 
 export interface ICardListProps {
-  characters: IData;
+  characters: Data | CustomData;
 }
 
 export interface ILocalStorage {
@@ -41,4 +43,83 @@ export interface ILocalStorage {
   clear(): void;
   removeItem(key: string): void;
   getAll(): Record<string, string>;
+}
+
+export interface IInputProps {
+  classes: {
+    container: string;
+    label: string;
+    input: string;
+    error?: string;
+  };
+  id: string;
+  reference: React.RefObject<HTMLInputElement>;
+  type: string;
+  label?: string;
+  errorMessage?: string;
+  accept?: string;
+  switcher?: boolean;
+  isValid?: boolean;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface ISelectProps {
+  classes: {
+    container: string;
+    label: string;
+    select: string;
+    option: string;
+    error?: string;
+  };
+  id: string;
+  reference: React.RefObject<HTMLSelectElement>;
+  values: string[];
+  options: string[];
+  label?: string;
+  errorMessage?: string;
+  isValid: boolean;
+  onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
+}
+
+export interface IButtonProps {
+  classes: {
+    container: string;
+    button: string;
+    message?: string;
+  };
+  type: 'button' | 'reset' | 'submit';
+  buttonText: string;
+  isDisabled: boolean;
+  showMessage?: boolean;
+  message?: string;
+}
+
+export interface ICustomDataElement {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  gender: string;
+  birthDate: string;
+  avatar: string;
+}
+
+export type CustomData = ICustomDataElement[];
+
+export interface IFormPageState {
+  cards: CustomData;
+}
+
+export interface IFormProps {
+  addNewCard: (card: ICustomDataElement) => void;
+}
+
+export interface IFormState {
+  buttonIsDisabled: boolean;
+  name: boolean;
+  species: boolean;
+  birthday: boolean;
+  avatar: boolean;
+  agreement: boolean;
+  showMessage: boolean;
 }
