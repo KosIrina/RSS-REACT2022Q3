792060b06531af761bbe987311ac4b2f716b4920
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-10 20:33:39 +0300

    feat: add input ui component and styles to it

---
+++ react-app/src/components/UI/Input/Input.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { IInputProps } from 'types';
+import './Input.css';
+
+class Input extends React.Component<IInputProps> {
+  constructor(props: IInputProps) {
+    super(props);
+  }
+
+  render(): JSX.Element {
+    return (
+      <div className={this.props.classes.container}>
+        <label htmlFor={this.props.id} className={this.props.classes.label}>
+          {this.props.label}
+          <input
+            type={this.props.type}
+            id={this.props.id}
+            className={this.props.classes.input}
+            accept={this.props.accept}
+            ref={this.props.reference}
+            onChange={this.props.onChange}
+          />
+          {this.props.switcher && <span className="swithcer-circle" />}
+        </label>
+        {!this.props.isValid && this.props.errorMessage && (
+          <span className={this.props.classes.error}>{this.props.errorMessage}</span>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Input;
