7676b94821a7ebcde487738481ccd1bd3b236033
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-10 21:11:32 +0300

    feat: update form page component(add necessary methods, child components); fix: update some main container styles

--- react-app/src/pages/FormPage/FormPage.tsx
+++ react-app/src/pages/FormPage/FormPage.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import Form from '../../components/Form';
+import CardList from '../../components/CardList';
+import { ICustomDataElement, IFormPageState, SearchBarProps } from '../../types';
 
-const FormPage = (): JSX.Element => {
-  return <div className="main-wrapper" />;
-};
+class FormPage extends React.Component<SearchBarProps, IFormPageState> {
+  constructor(props: SearchBarProps) {
+    super(props);
+    this.state = {
+      cards: [],
+    };
+    this.addNewCard = this.addNewCard.bind(this);
+  }
+
+  addNewCard(card: ICustomDataElement): void {
+    this.setState((state) => ({
+      cards: [card, ...state.cards],
+    }));
+  }
+
+  render(): JSX.Element {
+    return (
+      <div className="main-wrapper">
+        <h2 className="main__title">Here you can create your own character</h2>
+        {<Form addNewCard={this.addNewCard} />}
+        {<CardList characters={this.state.cards} />}
+      </div>
+    );
+  }
+}
 
 export default FormPage;
