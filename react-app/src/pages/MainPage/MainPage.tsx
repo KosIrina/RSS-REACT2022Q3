8c8a35da98f1ce9f9214e7bf79af4a0f36361d86
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-17 09:58:46 +0300

    feat: add displaying data/error message after mount, apply loader, add styles for error-message

--- react-app/src/pages/MainPage/MainPage.tsx
+++ react-app/src/pages/MainPage/MainPage.tsx
@@ -2,15 +2,80 @@ import React from 'react';
 import './MainPage.css';
 import SearchBar from '../../components/SearchBar';
 import CardList from '../../components/CardList';
-import CharactersInfo from '../../data/universe-characters.json';
+import Loader from '../../components/Loader';
+import CharactersAPI from '../../api/charactersApi';
+import { API_ERROR_MESSAGES, ONE_SECOND } from '../../constants';
+import { EmptyObject, IMainPageState, Numbers } from '../../types';
 
-const MainPage = (): JSX.Element => {
-  return (
-    <div className="main-wrapper">
-      {<SearchBar />}
-      {<CardList characters={CharactersInfo} />}
-    </div>
-  );
-};
+class MainPage extends React.Component<EmptyObject, IMainPageState> {
+  charactersApi: CharactersAPI;
+
+  constructor(props: EmptyObject) {
+    super(props);
+    this.state = {
+      cards: [],
+      isLoading: true,
+      errorMessage: null,
+    };
+    this.charactersApi = new CharactersAPI();
+    this.updateMainPageState = this.updateMainPageState.bind(this);
+  }
+
+  async componentDidMount(): Promise<void> {
+    await this.updateMainPageState();
+  }
+
+  async updateMainPageState(searchParameter?: string): Promise<void> {
+    // setTimeout below in this method is added to be able to see loader in case response comes quickly
+    if (searchParameter !== undefined) {
+      this.setState(() => ({
+        isLoading: true,
+        errorMessage: null,
+      }));
+    }
+    try {
+      const charactersData = searchParameter
+        ? await this.charactersApi.getCharacters({
+            name: searchParameter,
+            page: Numbers.One,
+          })
+        : await this.charactersApi.getCharacters();
+      setTimeout((): void => {
+        this.setState(
+          (): IMainPageState => ({
+            cards: charactersData,
+            isLoading: false,
+            errorMessage: null,
+          })
+        );
+      }, ONE_SECOND);
+    } catch (error) {
+      setTimeout((): void => {
+        this.setState(
+          (): IMainPageState => ({
+            cards: [],
+            isLoading: false,
+            errorMessage: error instanceof Error ? error.message : API_ERROR_MESSAGES.unknown,
+          })
+        );
+      }, ONE_SECOND);
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      <div className="main-wrapper">
+        {<SearchBar updateMainPageState={this.updateMainPageState} />}
+        {this.state.isLoading && <Loader />}
+        {this.state.errorMessage && (
+          <div className="main__error-message">{this.state.errorMessage}</div>
+        )}
+        {!!this.state.cards.length && !this.state.isLoading && !this.state.errorMessage && (
+          <CardList characters={this.state.cards} />
+        )}
+      </div>
+    );
+  }
+}
 
 export default MainPage;
