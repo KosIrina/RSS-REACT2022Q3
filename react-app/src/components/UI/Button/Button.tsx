b79b5ac5189d6a6be8f7ae8208668062e7dd04db
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-17 15:30:45 +0300

    fix: update button component

--- react-app/src/components/UI/Button/Button.tsx
+++ react-app/src/components/UI/Button/Button.tsx
@@ -5,7 +5,12 @@ import './Button.css';
 const Button = (props: IButtonProps): JSX.Element => {
   return (
     <div className={props.classes.container}>
-      <button type={props.type} className={props.classes.button} disabled={props.isDisabled}>
+      <button
+        type={props.type}
+        className={props.classes.button}
+        disabled={props.isDisabled}
+        onClick={props.onClick}
+      >
         {props.buttonText}
       </button>
       {props.showMessage && <span className={props.classes.message}>{props.message}</span>}
