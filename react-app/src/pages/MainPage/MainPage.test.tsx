40464e04612cab45d6ba413adb7210e2080a959a
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-10-21 06:25:26 +0300

    feat: add some more tests for api

--- react-app/src/pages/MainPage/MainPage.test.tsx
+++ react-app/src/pages/MainPage/MainPage.test.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
+import server from '../../mocks/server';
 import MainPage from '.';
+import { FAKE_CARDS_LENGTH, HTTP_STATUS_CODES, URL_BASE, URL_ENDPOINTS } from '../../constants';
 import { Numbers } from '../../types';
 
 describe('Main page', (): void => {
@@ -11,6 +15,41 @@ describe('Main page', (): void => {
 
     expect(loader).toBeInTheDocument();
     expect(screen.queryAllByTestId('main-page-character-card')).toHaveLength(Numbers.Zero);
+    expect(screen.queryByTestId('cards-error-message')).not.toBeInTheDocument();
+  });
+
+  it('Should render cards according to API on upload', async (): Promise<void> => {
+    render(<MainPage />);
+
+    const cards = await screen.findAllByTestId('main-page-character-card');
+    expect(cards.length).toEqual(FAKE_CARDS_LENGTH);
+    expect(screen.queryByTestId('cards-loader')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('cards-error-message')).not.toBeInTheDocument();
+  });
+
+  it('Should render error message on bad API requests', async (): Promise<void> => {
+    server.use(
+      rest.get(`${URL_BASE}/${URL_ENDPOINTS.character}`, (request, response, context) => {
+        return response(context.status(HTTP_STATUS_CODES.notFound));
+      })
+    );
+
+    render(<MainPage />);
+
+    const errorMessage = await screen.findByTestId('cards-error-message');
+    expect(errorMessage).toBeInTheDocument();
+    expect(screen.queryByTestId('cards-loader')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('main-page-character-card')).not.toBeInTheDocument();
+  });
+
+  it('Should render cards after search submit', async (): Promise<void> => {
+    render(<MainPage />);
+    const user = userEvent.setup();
+    await user.type(screen.getByPlaceholderText(/Search by name../i), 'rick{enter}');
+
+    const cards = await screen.findAllByTestId('main-page-character-card');
+    expect(cards).toHaveLength(Numbers.Two);
+    expect(screen.queryByTestId('cards-loader')).not.toBeInTheDocument();
     expect(screen.queryByTestId('cards-error-message')).not.toBeInTheDocument();
   });
 });
