2b25ada04bc66effbca8522458623ebf229979d8
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-11-01 04:13:25 +0300

    fix: remove using app state component to app

--- react-app/src/components/App/index.tsx
+++ react-app/src/components/App/index.tsx
@@ -5,20 +5,23 @@ import MainPage from '../../pages/MainPage';
 import AboutPage from '../../pages/AboutPage';
 import FormPage from '../../pages/FormPage';
 import PageNotFound from '../../pages/PageNotFound';
+import { AppState } from '../../state/AppState';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<MainPage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="form" element={<FormPage />} />
-          <Route path="404" element={<PageNotFound />} />
-          <Route path="*" element={<Navigate to="404" />} />
-        </Route>
-      </Routes>
-    </div>
+    <AppState>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MainPage />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="form" element={<FormPage />} />
+            <Route path="404" element={<PageNotFound />} />
+            <Route path="*" element={<Navigate to="404" />} />
+          </Route>
+        </Routes>
+      </div>
+    </AppState>
   );
 }
 
--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
-import { AppState } from './state/AppState';
 import './index.css';
 
 const root: ReactDOM.Root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="/kosirina-REACT2022Q3">
-      <AppState>
-        <App />
-      </AppState>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
